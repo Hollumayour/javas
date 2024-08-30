632cf0d9e94a3908b989b1d30fdb8ea070ea3daa
hollumayour <mayorjohn99@gmail.com>
2024-08-30 04:00:13 -0700

    first commit

---
+++ js/whitespace.js
@@ -0,0 +1,24 @@
+// let username;
+// username = window.prompt("Enter your username")
+// let result = username.startsWith(" ");
+
+// if (result) {
+//     console.log("Your username contains extra space, kindly remove it");
+// }else {
+//     console.log(`your username is ${username} `);
+// }
+
+
+
+// string slicing
+// const fullName = "Oluwamayowa John";
+// let firstName = fullName.slice(0, 8);
+
+// console.log(`Your username is ${firstName}`);
+
+
+let email;
+email = window.prompt("Enter your email");
+email = email.toUpperCase();
+
+console.log(email);
